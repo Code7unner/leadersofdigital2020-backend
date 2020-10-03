33b84a204a6119126706687a7b0ca73e18a43bc4
Code7unner <Cherbadzhimaxim@gmail.com>
2020-10-04 05:07:29 +1000

    Fix cors

--- cmd/main.go
+++ cmd/main.go
@@ -104,10 +104,10 @@ func runExternalServer(ctx context.Context, config *configs.Config, logger *zap.
 
 func cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, X-CSRF-Token")
-		w.Header().Set("Access-Control-Expose-Headers", "Authorization")
+		header := w.Header()
+		header.Add("Access-Control-Allow-Origin", "*")
+		header.Add("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
+		header.Add("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
 
 		next.ServeHTTP(w, r)
 	})
